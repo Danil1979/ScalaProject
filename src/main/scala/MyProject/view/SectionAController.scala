e0cb9c04e7f02dbba0427929261c7b3eb0042fb7
Hock Shem <hockshem.c@gmail.com>
2020-02-27 02:09:15 +0800

    Added textformatter

--- src/main/scala/MyProject/view/SectionAController.scala
+++ src/main/scala/MyProject/view/SectionAController.scala
@@ -3,17 +3,29 @@ package MyProject.view
 import MyProject.util.util.sectionAURL
 import javafx.scene.Parent
 import scalafx.scene.Scene
-import scalafx.scene.control.TextField
+import scalafx.scene.control.{TextField, TextFormatter}
 import scalafxml.core.macros.sfxml
+import scalafx.scene.control.TextFormatter.Change
 import scalafxml.core.{FXMLLoader, NoDependencyResolver}
-import scala.compat.java8.FunctionConverters._
+
 @sfxml
 class SectionAController(val income: TextField) {
   income.setText("hello")
 
-  def callSectionA(): Unit ={
-    println("Section A")
-
+  val filter: Change => Change = { change: Change =>
+    if (isLegitNumber(change.text)) change else null
   }
 
+  val formatter = new TextFormatter[String](filter)
+
+  income.textFormatter = formatter
+
+  def callSectionA(): Unit ={
+    println("Section A")
+  }
+
+  // Accepts negative sign but not leading zeros
+  def isLegitNumber(input: String): Boolean = {
+    input.matches("-?(0|[1-9]\\\\d*)")
+  }
 }
